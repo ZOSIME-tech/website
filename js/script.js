d53668d80835e66de61a20d0edf8b61086fac934
Pranav <pranav.gnanasekar23@gmail.com>
2020-07-21 12:52:08 +0530

    sticky nav add

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".project").waypoint(function (direction) {
+    if (direction == "down") {
+      $("nav").addClass("sticky");
+    } else {
+      $("nav").removeClass("sticky");
+    }
+  });
+});
