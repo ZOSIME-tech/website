41c8f5fc92729df4d309f1af80c09f8a172c1eb8
Pranav <pranav.gnanasekar23@gmail.com>
2020-07-22 20:47:11 +0530

    media query manager added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
-  $(".project").waypoint(function (direction) {
+  $("nav").waypoint(function (direction) {
     if (direction == "down") {
       $("nav").addClass("sticky");
+      $(".nav").css("padding", "1.5rem 0");
     } else {
       $("nav").removeClass("sticky");
+      $(".nav").css("padding", "3rem 0");
     }
+  }, {
+    offset: '-4%'
   });
 });
