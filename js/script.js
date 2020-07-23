2d4756733564e05decd44b2659462df1def035bd
Pranav <pranav.gnanasekar23@gmail.com>
2020-07-23 11:56:39 +0530

    sticky nav bug

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
-  $("nav").waypoint(function (direction) {
-    if (direction == "down") {
-      $("nav").addClass("sticky");
-      $(".nav").css("padding", "1.5rem 0");
-    } else {
-      $("nav").removeClass("sticky");
-      $(".nav").css("padding", "3rem 0");
+  $("nav").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+        $(".nav").css("padding", "1.5rem 0");
+      } else {
+        $("nav").removeClass("sticky");
+        $(".nav").css("padding", "3rem 0");
+      }
+    },
+    {
+      offset: "-4%",
     }
-  }, {
-    offset: '-4%'
-  });
+  );
 });
