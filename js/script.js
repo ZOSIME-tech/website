b820aa5312942615a328df31bf30e8d470fe0389
Pranav <pranav.gnanasekar23@gmail.com>
2020-07-23 12:06:36 +0530

    sticky nav glitch fix

--- js/script.js
+++ js/script.js
@@ -3,10 +3,8 @@ $(document).ready(function () {
     function (direction) {
       if (direction == "down") {
         $("nav").addClass("sticky");
-        $(".nav").css("padding", "1.5rem 0");
       } else {
         $("nav").removeClass("sticky");
-        $(".nav").css("padding", "3rem 0");
       }
     },
     {
